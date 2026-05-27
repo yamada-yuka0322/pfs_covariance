c1e5d591c8b9d2a58f8531ab7664a9f76187de9f
yamada-yuka0322 <YukaYamada@idark.ipmu.jp>
2026-05-27 17:24:33 +0900

    Initial implementation

---
+++ src/pfscovariance/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#__all__ = [""]
+
+__author__ = "Yuka Yamada"
+__email__ = "yamada-yuka0322@g.ecc.u-tokyo.ac.jp"
+__uri__ = "https://github.com/yamada-yuka0322/pfs_covariance"
+__license__ = "MIT"
+__description__ = "Covariance matrix for the PFS Cosmology Survey"
+__copyright__ = "Copyright 2026 Yuka Yamada"
